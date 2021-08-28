1b3440a1d17b2b59631841b5d8ccad155f75c1f7
olci34 <developerogulcan@gmail.com>
2021-08-27 21:37:16 -0400

    add validations to user model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,8 +2,4 @@ class Trip < ApplicationRecord
     belongs_to :user
     has_one :ticket
     has_and_belongs_to_many :planets
-    
-    def planets_attributes=(attributes)
-        binding.pry
-    end
 end
