e2928f48ff3f4d16e4ce5de02ae889d9906c89ba
olci34 <developerogulcan@gmail.com>
2021-09-03 23:37:58 -0400

    adelete ticket along with its trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,13 @@ class Trip < ApplicationRecord
     has_one :ticket
     has_and_belongs_to_many :planets
     accepts_nested_attributes_for :ticket
+    after_destroy :destroyTripTicket
+
+    private
+
+    def destroyTripTicket
+        binding.pry
+        self.ticket.destroy
+    end
+
 end
